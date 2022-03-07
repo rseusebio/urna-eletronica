e03b055320df239742f2db0b2786fa0b858f4ba6
rseusebio <rafaelsantos.rse@gmail.com>
2022-03-07 16:31:04 -0300

    added front

--- database/my_script.py
+++ database/my_script.py
@@ -12,7 +12,7 @@ def read_politics(file_name: str) -> None:
             canditados = v["candidatos"]
             for id, obj in canditados.items():
                 vice = obj["vice"] if "vice" in obj.keys() else {}
-                insert_str = """INSERT INTO Politicos (ID, titulo, Nome, Partido, Foto, Votos, Vice) VALUES ({0}, '{1}', '{2}', '{3}', '{4}', {5}, '{6}');"""\
+                insert_str = """INSERT INTO Politicos (ID, Titulo, Nome, Partido, Foto, Votos, Vice) VALUES ({0}, '{1}', '{2}', '{3}', '{4}', {5}, '{6}');"""\
                     .format(id, titulo, obj["nome"], obj["partido"], obj["foto"], "0", json.dumps(vice))\
                     .strip()\
                     .strip('\n')
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     with open(os.path.abspath("./create_all.sql"), "w") as f:
         data = read_politics(os.path.abspath("./politicos.json"))
         f.write("use MYSQL_DATABASE;\n")
-        f.write("CREATE TABLE Politicos (ID INT, titulo VARCHAR(255), Nome VARCHAR(255), Partido VARCHAR(255), Foto VARCHAR(255), Votos INT, Vice LONGTEXT);\n")
-        f.write("INSERT INTO Politicos (ID, titulo, Nome, Partido, Foto, Votos, Vice) VALUES (0, 'prefeito', 'BRANCO', '', '', 0, '');\n")
-        f.write("INSERT INTO Politicos (ID, titulo, Nome, Partido, Foto, Votos, Vice) VALUES (0, 'vereador', 'BRANCO', '', '', 0, '');\n")
-        f.write("INSERT INTO Politicos (ID, titulo, Nome, Partido, Foto, Votos, Vice) VALUES (-1, 'prefeito', 'NULO', '', '', 0, '');\n")
-        f.write("INSERT INTO Politicos (ID, titulo, Nome, Partido, Foto, Votos, Vice) VALUES (-1, 'vereador', 'NULO', '', '', 0, '');\n")
+        f.write("CREATE TABLE Politicos (ID INT, Titulo VARCHAR(255), Nome VARCHAR(255), Partido VARCHAR(255), Foto VARCHAR(255), Votos INT, Vice LONGTEXT);\n")
+        f.write("INSERT INTO Politicos (ID, Titulo, Nome, Partido, Foto, Votos, Vice) VALUES (0, 'prefeito', 'BRANCO', '', '', 0, '');\n")
+        f.write("INSERT INTO Politicos (ID, Titulo, Nome, Partido, Foto, Votos, Vice) VALUES (0, 'vereador', 'BRANCO', '', '', 0, '');\n")
+        f.write("INSERT INTO Politicos (ID, Titulo, Nome, Partido, Foto, Votos, Vice) VALUES (-1, 'prefeito', 'NULO', '', '', 0, '');\n")
+        f.write("INSERT INTO Politicos (ID, Titulo, Nome, Partido, Foto, Votos, Vice) VALUES (-1, 'vereador', 'NULO', '', '', 0, '');\n")
         f.write(data)
