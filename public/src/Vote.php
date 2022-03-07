02402941044316b5dd5ef54239a8ea50c8739b8d
rseusebio <rafaelsantos.rse@gmail.com>
2022-03-07 16:31:22 -0300

    added frond and fixed update operation;

--- public/src/Vote.php
+++ public/src/Vote.php
@@ -39,79 +39,74 @@ class Vote {
     }
   }
 
-  private function getTest()
-  {
-    $response['status_code_header'] = 'HTTP/1.1 200 OK';
-    
-    $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
-
-    $response['body'] = json_encode($age);
-
-    return $response;
-  }
-
   private function getAllVotes()
   {
-    $query = "SELECT * FROM Politicos;";
+    $query = "SELECT * FROM Politicos WHERE Titulo = 'prefeito';";
+    $statement = $this->db->query($query);
+    $prefeitos = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-    try {
-      $statement = $this->db->query($query);
-      $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
-    } catch (\PDOException $e) {
-      exit($e->getMessage());
-    }
+    $query = "SELECT * FROM Politicos WHERE Titulo  ='vereador';";
+    $statement = $this->db->query($query);
+    $vereadores = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+    $result = array(
+      "prefeitos" => $prefeitos,
+      "vereadores" => $vereadores
+    );
 
     $response['status_code_header'] = 'HTTP/1.1 200 OK';
     $response['body'] = json_encode($result);
+
     return $response;
   }
 
-  private function getPolitician($id) 
+  private function getPolitician($id, $title) 
   {
-    $query = "SELECT * FROM Politicos WHERE ID = :id;";
+    $query = "SELECT * FROM Politicos WHERE ID = :id AND Titulo = :titulo;";
 
     $stmt = $this->db->prepare($query);
-    $stmt->execute(array('id' => (int) $id));
+    $stmt->execute(array('id' => (int) $id, 'titulo' => $title));
 
     $politician = $stmt->fetch();
 
     return $politician;
   }
 
-  private function updateVote($id, $vote) 
+  private function updateVote($id, $vote, $title) 
   {
-    $update = "UPDATE Politicos SET Votos = :vote WHERE ID = :id;";
+    $update = "UPDATE Politicos SET Votos = :vote WHERE ID = :id and Titulo = :titulo;";
 
     $stmt = $this->db->prepare($update);
 
-    $stmt->execute(array( 'vote' => $vote , 'id' => $id));
+    $stmt->execute(array( 'vote' => $vote , 'id' => $id, 'titulo' => $title));
 
     return $stmt->rowCount();
   }
 
-  private function insertVote($id)
-  {
-    $politician = $this->getPolitician($id);
+  private function insertVote($id, $title)
+ {
+    $politician = $this->getPolitician($id, $title);
 
-    if (!$politician) 
-    {
+    if (!$politician) {
       return array("message" => "Couldn't find a politician with ID " . $id, "success" => false );
     }
 
     $vote = 1 + (int) $politician['Votos'];
 
-    $rows = $this->updateVote($id, $vote);
+    $rows = $this->updateVote($id, $vote, $title);
 
-    $new_pol = $this->cleanObject($this->getPolitician($id));
-
-    return array("politician" => $new_pol, "success"=> true);
+    return array(
+      "politician" => $this->cleanObject($this->getPolitician($id, $title)),
+      "rowsUpdated" => $rows, 
+      "success"=> true
+    );
   }
 
   public function postHandler($vereador_id, $prefeito_id) 
   {
       $result = array(
-        "vereador" => $this->insertVote($vereador_id),
-        "prefeito" => $this->insertVote($prefeito_id)
+        "vereador" => $this->insertVote($vereador_id, "vereador"),
+        "prefeito" => $this->insertVote($prefeito_id, "prefeito")
       );
 
       $response['status_code_header'] = 'HTTP/1.1 200 OK';
